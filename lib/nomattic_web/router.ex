a676691abc9db2be4e4c9f1658e63da31bfb7472
Mike Nomitch <mnomitch@hashicorp.com>
2021-06-01 23:29:17 -0700

    Adding custom scss

--- lib/nomattic_web/router.ex
+++ lib/nomattic_web/router.ex
@@ -24,9 +24,9 @@ defmodule NomatticWeb.Router do
   end
 
   # Other scopes may use custom stacks.
-  # scope "/api", NomatticWeb do
-  #   pipe_through :api
-  # end
+  scope "/api", NomatticWeb do
+    pipe_through :api
+  end
 
   # Enables LiveDashboard only for development
   #
